8fe36fdc37b1748522da765fbb7da74ead800a58
unknown <gustavogoetten87@gmail.com>
2024-08-31 14:13:53 -0300

    :construction: Parte 2 fazendo os calculos requisitados no enunciado

--- Fase 1/main.py
+++ Fase 1/main.py
@@ -5,22 +5,48 @@ Agradeço o tempo que tiraram para corrigir o meu código :) """
 # Parte 1: Validação dos dados que irão entrar no código
 
 # Lista que irá armazenar todas as temperaturas 
-TemperaturaDeCadaMes = []
+temps = []
 
-# Laço para receber todas as temperaturas 
+# Loop para receber as temperaturas 
 for mes in range(1, 13): 
-    # O loop abaixa irá receber e validar os dados de entrada
+
     while True:
-        # Requisitando ao usuario as temperaturas 
+        
         temperatura = float(input(f"Digite a temperatura máxima do mês {mes}: "))
 
         # Verificando se a temperatura é valida 
         if temperatura >= -60 and temperatura <= 50:
-            # Se a temperatura estiver correta, usamos o códico abaixo para adicionar a lista de temperaturas de cada mes 
-            TemperaturaDeCadaMes.append(temperatura)
+            
+            temps.append(temperatura)
 
             break
         else:
-            # Se a temperatura estiver fora dos padrões colocamos a seguinte mensagem 
             print("A temperatura é invalida, insira uma medida entre -60 a 50 Graus Celsius")
+
+# Parte 2: Calculos requisitados no enunciado 
+
+# Colocando os meses por extenso 
+meses_escritos = ["janeiro", "fevereiro", "março", "abril", "maio", "junho", "julho", "agosto", "setembro", "outubro", "novembro", "dezembro"]
+
+# Temperatura média máxima anual
+media_maxima = sum(temps) / 12
+print(f"\nTemperatura média máxima anual: {media_maxima: .2f}°C")
+
+# Meses que tivera temperatura acima dos 33°C 
+meses_escaldantes = 0
+for temperatura in temps:
+    if temperatura > 33:
+        meses_escaldantes += 1
+
+print(f"Quantidade de meses escaldantes (temperatura acima de 33°C): {meses_escaldantes}")
+
+# Mes com a maior temperatura 
+mes_mais_quente = max(temps)
+mes_mais_quente_nome = temps.index(mes_mais_quente)
+print(f"O mes mais escaldante foi {meses_escritos[mes_mais_quente_nome]} com {mes_mais_quente}°C")
+
+# Mes com a menor temperatura
+mes_menos_quente = min(temps)
+mes_menos_quente_nome = temps.index(mes_menos_quente)
+print(f"O mes mais frio foi {meses_escritos[mes_menos_quente_nome]} com {mes_menos_quente}°C")
 
